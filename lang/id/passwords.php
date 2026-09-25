b1128ff0b1b62b3bcc3c024dea680145e1c9f1f8
Realitaa <rezamuliaputra927@gmail.com>
2026-09-25 20:27:28 +0700

    feat: publish and create id translation

---
+++ lang/id/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris Bahasa Atur Ulang Kata Sandi (Password Reset Language Lines)
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut adalah baris default yang cocok dengan alasan yang
+    | diberikan oleh broker kata sandi untuk hasil percobaan pembaruan kata sandi
+    | seperti kegagalan karena kata sandi / token atur ulang tidak valid.
+    |
+    */
+
+    'reset' => 'Kata sandi Anda telah diatur ulang.',
+    'sent' => 'Kami telah mengirimkan tautan atur ulang kata sandi Anda melalui email.',
+    'throttled' => 'Harap tunggu sebelum mencoba kembali.',
+    'token' => 'Token atur ulang kata sandi ini tidak valid.',
+    'user' => 'Kami tidak dapat menemukan pengguna dengan alamat email tersebut.',
+
+];
